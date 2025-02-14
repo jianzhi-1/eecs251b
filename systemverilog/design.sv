88873a9eb6fd60ff524e91b43ef1fb76b7aeca9a
Jianzhi Wang <jianzhi@berkeley.edu>
2025-02-13 21:44:03 -0800

    Update design.sv

--- systemverilog/design.sv
+++ systemverilog/design.sv
@@ -1,16 +1,16 @@
 module d_ff(clk, resetn, q, d);
 
-	input clk;
-	input resetn;
-	input d;
-	output q;
+    input clk;
+    input resetn;
+    input d;
+    output q;
 
-	reg q;
+    reg q;
 
-	always @ (posedge clk)
-		if (! resetn)
-			q <= 0;
-		else
-			q <= d;
+    always @ (posedge clk)
+        if (! resetn)
+            q <= 0;
+        else
+            q <= d;
 
 endmodule
