e47745e05fd8d801a1b07871728a2082ba18b00b
Jianzhi Wang <jianzhi@berkeley.edu>
2025-02-18 16:52:20 -0800

    Update verification_top.sv

--- systemverilog/verification_top.sv
+++ systemverilog/verification_top.sv
@@ -1,3 +1,23 @@
+class Packet;
+    bit [31:0] addr;
+    bit [31:0] payload [4];
+
+    function new (bit [31:0] addr);
+        this.addr = addr;
+        this.payload = `{0, 0, 0, 0};
+    endfunction
+endclass
+
+class RandomPacket;
+    rand bit [7:0] min, max;
+    constraint range {
+        0 < min; max < 128; min < max;
+    }
+    extern constraint extra;
+endclass
+
+constraint RandomPacket::extra { min > 5; };
+
 module verification_top();
     reg clk;
     reg resetn;
@@ -24,6 +44,12 @@ module verification_top();
         arr = new[arr.size()+1](arr);
         arr[2] = 4;
         arr.delete();
+
+        pkt = new Packet(32'd4);
+        $display(pkt.payload);
+        
+        cls = new();
+        cls.randomize() with { min == 10; } // utilises external constraint
         
         resetn <= 0;
         d <= 0;
